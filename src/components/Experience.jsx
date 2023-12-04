a67a1c10792be86d27d2abeb604a2008a529d876
Miguel Pedraza <mikeped469@gmail.com>
2023-12-04 14:12:29 -0600

    Intial project with r4f, drei, three-stdlib with model character set

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,16 @@
-import { OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+
+import { HoodieCharacter } from "./HoodieCharacter";
 
 export const Experience = () => {
-  return (
-    <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </>
-  );
+	return (
+		<>
+			<Environment preset="sunset" />
+			<ambientLight intensity={0.3} />
+			<ContactShadows blur={2} />
+			<OrbitControls />
+			<HoodieCharacter />
+			<HoodieCharacter position-x={1} hairColor="red" topColor="blue" />
+		</>
+	);
 };
