cf52f1190fed6c8d494b772df17c9a45f1772576
Miguel Pedraza <mikeped469@gmail.com>
2023-12-04 16:19:59 -0600

    Finished socket io connection creating new characters with any new people visiting

--- src/components/SocketManager.jsx
+++ src/components/SocketManager.jsx
@@ -4,10 +4,20 @@ import { useEffect } from "react";
 // Socket.io-client imports
 import { io } from "socket.io-client";
 
+// Jotai imports
+import { atom, useAtom } from "jotai";
+
 // Create the socket io connection
 export const socket = io("http://localhost:3500");
 
+// Global state for characters
+export const charactersAtom = atom([]);
+
 const SocketManager = () => {
+	// State
+	const [characters, setCharacters] = useAtom(charactersAtom);
+
+	// Effect for socket connection
 	useEffect(() => {
 		const onConnect = () => {
 			console.log("Connected!");
@@ -21,20 +31,24 @@ const SocketManager = () => {
 			console.log("Hello!");
 		};
 
+		const onCharacters = (value) => {
+			setCharacters(value);
+		};
+
 		// Add function actions to actions
 		socket.on("connect", onConnect);
 		socket.on("disconnect", onDisconnect);
 		socket.on("hello", onHello);
+		socket.on("characters", onCharacters);
 
 		// Clean up (un mount)
 		return () => {
 			socket.off("connect", onConnect);
 			socket.off("disconnect", onConnect);
 			socket.off("hello", onConnect);
+			socket.off("characters", onCharacters);
 		};
 	}, []);
-
-	return <div>SocketManager</div>;
 };
 
 export default SocketManager;
